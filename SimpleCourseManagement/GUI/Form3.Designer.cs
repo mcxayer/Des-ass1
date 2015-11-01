7f7075b10c5e47214b2bc956ef30911665785360
mcxayer <niels.bonde.nielsen@hotmail.com>
2015-11-01 18:31:57 +0100

    exam and grades

--- SimpleCourseManagement/GUI/Form3.Designer.cs
+++ SimpleCourseManagement/GUI/Form3.Designer.cs
@@ -51,7 +51,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(260, 68);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Exam";
+            this.button2.Text = "Exam and Grades";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
