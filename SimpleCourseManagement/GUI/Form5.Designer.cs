169887e6219e9ba4ca2426f93e1437fbc8ee743e
mcxayer <niels.bonde.nielsen@hotmail.com>
2015-11-01 19:20:34 +0100

    Escape the matrix is back

--- SimpleCourseManagement/GUI/Form5.Designer.cs
+++ SimpleCourseManagement/GUI/Form5.Designer.cs
@@ -55,9 +55,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(411, 387);
+            this.button2.Location = new System.Drawing.Point(371, 387);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(113, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Escape the Matrix";
             this.button2.UseVisualStyleBackColor = true;
